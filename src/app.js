b490f0036266be8b92867d5f693d3da3d5c638bf
iuliia <popova.iuliia.iu@gmail.com>
2022-12-02 16:03:01 +0300

    Add i18n

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 import * as yup from 'yup';
 import onChange from 'on-change';
-import render, { elements } from './view.js';
+import render, { renderText, elements } from './view.js';
+import i18n from 'i18next';
+import resources from './locales/index.js';
+
+// const messages = {
+//   url: 'Ссылка должна быть валидным URL',
+//   notOneOf: 'RSS уже существует',
+//   success: 'RSS успешно загружен',
+// };
 
 const validate = (url, urls) => yup.string().required().url()
   .notOneOf(urls)
@@ -8,12 +16,21 @@ const validate = (url, urls) => yup.string().required().url()
 
 export default () => {
   const state = {
+    lng: 'en',
     process: 'filling',
     urls: [],
     feedbackText: null,
   };
 
-  const watchedState = onChange(state, render(elements));
+  const i18next = i18n.createInstance();
+  i18next.init({
+    lng: state.lng,
+    debug: false,
+    resources,
+  })   
+  renderText(elements, i18next);
+
+  const watchedState = onChange(state, render(elements, i18next));
 
   elements.form.addEventListener('submit', ((event) => {
     event.preventDefault();
@@ -26,10 +43,11 @@ export default () => {
         watchedState.process = 'success';
         watchedState.feedbackText = 'success';
         watchedState.urls.push(url);
+        
       })
       .catch((err) => {
         watchedState.process = 'error';
-        watchedState.feedbackText = err.type;
+        watchedState.feedbackText = err.type;        
       });
-  }));
+  })); 
 };
--- src/view.js
+++ src/view.js
@@ -5,16 +5,23 @@ export const elements = {
   feedback: document.querySelector('.feedback'),
 };
 
-const messages = {
-  url: 'Ссылка должна быть валидным URL',
-  notOneOf: 'RSS уже существует',
-  success: 'RSS успешно загружен',
+export const renderFeedback = (elements, i18next, feedbackText) => {
+  const { feedback } = elements;
+  feedback.textContent = i18next.t(`messages.${feedbackText}`);  
 };
 
-export const renderFeedback = (elements, feedbackText) => {
-  const { feedback } = elements;
-  feedback.textContent = messages[feedbackText];  
-};
+export const renderText = (elements, i18next) => {
+  const { form, button } = elements;
+  const headline =  document.querySelector('.display-3 ');
+  headline.textContent = i18next.t(`headline`); 
+  const subtitle = document.querySelector('.lead');
+  subtitle.textContent = i18next.t(`subtitle`); 
+  const placeholder = form.querySelector('[for="url-input"]');
+  placeholder.textContent = i18next.t(`placeholder`);  
+  const example = document.querySelector('.example');
+  example.textContent = i18next.t(`example`); 
+  button.textContent = i18next.t(`button`); 
+}
 
 const handleProcessState = (elements, process) => {
   const { form, input, feedback } = elements;
@@ -28,26 +35,25 @@ const handleProcessState = (elements, process) => {
       input.classList.remove('is-invalid');
       form.reset();
       input.focus();
-    break;
+      break;
     case 'error':
       input.classList.remove('is-valid');
       input.classList.add('is-invalid');
       feedback.classList.remove('text-success');
       feedback.classList.add('text-danger');
       break;        
-    default:
-      
+    default:      
       break;
   }
 };
 
-export default (elements) => (path, value) => {
+export default (elements, i18next) => (path, value) => {
   switch (path) {
     case 'process':
-      handleProcessState(elements, value);
+      handleProcessState(elements, value,);
       break;
     case 'feedbackText':
-      renderFeedback(elements, value);
+      renderFeedback(elements, i18next, value,);
       break;
     default:
       break;
